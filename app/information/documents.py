4051170ecc980c4b35ab6f2e9a5bd7c46e83f31b
DoHyang Kim <hanoul1124@gmail.com>
2019-06-11 00:09:08 +0900

    Elasticsearch Added for Open API v2.5

--- app/information/documents.py
+++ app/information/documents.py
@@ -1,5 +1,5 @@
 from django_elasticsearch_dsl import Index, DocType, fields
-from elasticsearch_dsl import connections
+from elasticsearch_dsl import connections, analyzer
 from .models import *
 
 # Define a default Elasticsearch client
@@ -14,27 +14,58 @@ fni_index.settings(
     number_of_replicas=0
 )
 
+nori_analyzer = analyzer(
+    'nori_analyzer',
+    tokenizer="nori_tokenizer"
+)
 
 @fni_index.doc_type
 class FNIDocument(DocType):
+    id = fields.IntegerField(attr='pk')
+    food_name = fields.StringField(
+        # analyzer 로 사용할 것 지정
+        analyzer=nori_analyzer,
+        # field_name.raw = analyzer를 keyword(built-in analyzer)로 사용한
+        # 결과를 돌려준다.
+        fields={
+            'raw': fields.StringField(analyzer='keyword'),
+        }
+    )
+    food_group = fields.StringField(
+        analyzer=nori_analyzer,
+        fields={
+            'raw': fields.StringField(analyzer='keyword'),
+        }
+    )
+    food_amount = fields.StringField()
+    calorie = fields.StringField()
+    carbs = fields.StringField()
+    protein = fields.StringField()
+    fat = fields.StringField()
+    sugar= fields.StringField()
+    salt= fields.StringField()
+    cholesterol = fields.StringField()
+    saturated_fatty_acid = fields.StringField()
+    trans_fat = fields.StringField()
+
     class Meta:
         # The model associated with this DocType
         model = FNI
         # The fields of the model you want to be indexed in Elasticsearch
-        fields = [
-            'food_name',
-            'food_group',
-            'food_amount',
-            'calorie',
-            'carbs',
-            'protein',
-            'fat',
-            'sugar',
-            'salt',
-            'cholesterol',
-            'saturated_fatty_acid',
-            'trans_fat'
-        ]
-
-    id = fields.IntegerField(attr='pk')
+    #     fields = [
+    #         'food_name',
+    #         'food_group',
+    #         'food_amount',
+    #         'calorie',
+    #         'carbs',
+    #         'protein',
+    #         'fat',
+    #         'sugar',
+    #         'salt',
+    #         'cholesterol',
+    #         'saturated_fatty_acid',
+    #         'trans_fat'
+    #     ]
+    #
+    # id = fields.IntegerField(attr='pk')
 
